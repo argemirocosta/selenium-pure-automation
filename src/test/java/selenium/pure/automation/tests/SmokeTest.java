beb7f03edcfe7f2ebba35c47c09899357676bac7
Argemiro github <argemirocosta@outlook.com>
2026-09-25 11:58:13 -0300

    Removing display name from smoke test

--- src/test/java/selenium/pure/automation/tests/SmokeTest.java
+++ src/test/java/selenium/pure/automation/tests/SmokeTest.java
@@ -1,6 +1,5 @@
 package selenium.pure.automation.tests;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import selenium.pure.automation.core.Config;
@@ -8,11 +7,9 @@ import selenium.pure.automation.core.Config;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Tag("smoke")
-@DisplayName("Smoke")
 class SmokeTest extends BaseTest {
 
     @Test
-    @DisplayName("Home page abre com o título esperado")
     void shouldOpenHomePage() {
         driver.get(Config.baseUrl());
 
